55b84921d973cb9d8e2228af1967e21207750f72
Haruna Kaze <haruna.kaze@gmail.com>
2016-07-04 16:34:12 +0700

    - Refactor request server

--- NetCopy/ServerRequest.cs
+++ NetCopy/ServerRequest.cs
@@ -11,23 +11,60 @@ namespace NetCopy {
         private const string REQUEST_QUERY = "!@#REQUEST_DATA#@!<!@#!@#)?>";
         private const string CHECK_QUERY = "!@#CHECK_DATA#@!<!@#!@#)?>";
         private const int REQUST_TIMEOUT_MS = 200;
-        private string pasteData;
         private IPAddress serverIP;
 
         public ServerRequest(IPAddress serverIP) {
             this.serverIP = serverIP;
-            ServerIsAvailable();
+            //ServerIsAvailable();
             RequestServerData();
         }
 
-        private void ServerIsAvailable() {
+        //private void ServerIsAvailable() {
+        //    Console.WriteLine("    \nCHECKING SERVER...");
+
+        //    NetworkClient netClient = new NetworkClient();
+
+        //    string serverResponse = null;
+
+        //    Thread clientThread = new Thread(() => netClient.StartClient(serverIP, CHECK_QUERY, out serverResponse));
+        //    clientThread.Start();
+
+        //    //TODO: Find other method of waiting this process, so it make a responsive impression?
+        //    clientThread.Join(REQUST_TIMEOUT_MS);
+
+        //    if (serverResponse == null) {
+        //        throw new ServerOfflineException();
+        //    }
+        //}
+
+        public string RequestServerData() {
+            //Console.WriteLine("    \nREQUESTING DATA...");
+
+            //NetworkClient netClient = new NetworkClient();
+
+            //string pasteData = null;
+
+            //Thread clientThread = new Thread(() => netClient.StartClient(serverIP, REQUEST_QUERY, out pasteData));
+            //clientThread.Start();
+
+            ////TODO: Find other method of waiting this process, so it make a responsive impression?
+            //clientThread.Join(REQUST_TIMEOUT_MS);
+
+            //Console.WriteLine("DONE REQUESTING DATA, DATA : " + pasteData);
+
+            //return pasteData;
+
+            return MakeRequest(REQUEST_QUERY);
+        }
+
+        private string MakeRequest(string request) {
             Console.WriteLine("    \nCHECKING SERVER...");
 
             NetworkClient netClient = new NetworkClient();
 
             string serverResponse = null;
 
-            Thread clientThread = new Thread(() => netClient.StartClient(serverIP, CHECK_QUERY, out serverResponse));
+            Thread clientThread = new Thread(() => netClient.StartClient(serverIP, request, out serverResponse));
             clientThread.Start();
 
             //TODO: Find other method of waiting this process, so it make a responsive impression?
@@ -36,22 +73,8 @@ namespace NetCopy {
             if (serverResponse == null) {
                 throw new ServerOfflineException();
             }
-        }
 
-        public string RequestServerData() {
-            Console.WriteLine("    \nREQUESTING DATA...");
-
-            NetworkClient netClient = new NetworkClient();
-
-            Thread clientThread = new Thread(() => netClient.StartClient(serverIP, REQUEST_QUERY, out pasteData));
-            clientThread.Start();
-
-            //TODO: Find other method of waiting this process, so it make a responsive impression?
-            clientThread.Join(REQUST_TIMEOUT_MS);
-
-            Console.WriteLine("DONE REQUESTING DATA, DATA : " + pasteData);
-
-            return pasteData;
+            return serverResponse;
         }
     }
 }
